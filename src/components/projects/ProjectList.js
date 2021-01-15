0675f2e4df182e89817e6e4ede1b5ed22b6ebf58
mazibinfo <mazibulyah@gmail.com>
2021-01-15 12:30:28 +0600

    Dashboard Component

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return(
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summery">
+                <span className="card-title">Project Title</span>
+                <p>Posted By Ninja</p>
+                <p className="grey-text">1st Jan, 2021</p>
+            </div>
+
+            <div className="card z-depth-0 project-summery">
+                <span className="card-title">Project Title</span>
+                <p>Posted By Ninja</p>
+                <p className="grey-text">1st Jan, 2021</p>
+            </div>
+
+            <div className="card z-depth-0 project-summery">
+                <span className="card-title">Project Title</span>
+                <p>Posted By Ninja</p>
+                <p className="grey-text">1st Jan, 2021</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList
